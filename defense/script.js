1d72354069938c1c4a23a8677f52647c1791fc1b
Acei-png <132578334+Acei-png@users.noreply.github.com>
2024-02-28 06:02:31 -0800

    Delete defense directory

--- defense/script.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('.header');
-
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
-    header.classList.add('header-scrolled');
-  } else if (window.scrollY <= 50){
-    header.classList.remove('header-scrolled')
-  }
-});
